baeeeca78e8bdd24b2b7c2011fb472b7fcf0762a
phkhang <trannguyenphuckhang12@gmail.com>
2025-10-04 14:51:57 +0700

    feat: add experiment view

--- src/pages/NeuControlPanel.jsx
+++ src/pages/NeuControlPanel.jsx
@@ -45,7 +45,7 @@ const hintQueries = [
   'Dark energy'
 ];
 
-export default function SpaceshipInteriorNeumorphic() {
+export default function SpaceshipInteriorNeumorphicc() {
   const [searchQuery, setSearchQuery] = useState('');
   const [activeControls, setActiveControls] = useState({
     power: true,
